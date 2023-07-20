9243ea552c066589f3fce10c145f54f4eec59635
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-07-19 20:02:33 -0500

    Update tests and readme

--- src/tests/listItem.test.tsx
+++ src/tests/listItem.test.tsx
@@ -1,6 +1,51 @@
 // Imports
 import { describe, it, expect, vi } from "vitest";
 import * as handler from "../handlers/list";
+import ListItem from "../components/listItem";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("list item component tests", async () => {
+  afterEach(() => {
+    cleanup();
+    vi.restoreAllMocks();
+  });
+
+  beforeEach(() => {
+    render(
+      <ListItem
+        title={"test_title"}
+        description={"test_description"}
+        index={"1"}
+        todo={[]}
+        setTodo={() => {}}
+        setIdToEdit={() => {}}
+      />
+    );
+  });
+
+  it("Should call the edit handler on edit", async () => {
+    const user = userEvent.setup();
+    const editTaskSpy = vi.spyOn(handler, "editTask");
+    const editButton = await screen.findByRole("button", {
+      name: "Edit",
+    });
+    await user.click(editButton as HTMLElement);
+
+    expect(editTaskSpy).toBeCalled();
+  });
+
+  it("Should call the delete handler on delete", async () => {
+    const user = userEvent.setup();
+    const deleteTaskSpy = vi.spyOn(handler, "deleteTask");
+
+    const deleteButton = await screen.findByRole("button", {
+      name: "Delete",
+    });
+    await user.click(deleteButton as HTMLElement);
+    expect(deleteTaskSpy).toBeCalled();
+  });
+});
 
 describe("Tests list item handlers", async () => {
   it("Should update state for id to edit", async () => {
