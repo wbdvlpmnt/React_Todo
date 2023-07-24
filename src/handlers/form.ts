13d2916c30071fa5cbe069b66212090b59953257
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-07-24 13:20:08 -0500

    save get and edit working

--- src/handlers/form.ts
+++ src/handlers/form.ts
@@ -1,7 +1,9 @@
+import { postNewTask } from "../services/service";
 import { Todo } from "../types/types";
+const baseURL = "http://localhost:3000/todoapi";
 
 export async function submitTask(
-  idToEdit: string,
+  idToEdit: number,
   title: string,
   description: string,
   todo: Todo[],
@@ -11,20 +13,29 @@ export async function submitTask(
   if (title.length > 0 && description.length > 0) {
     if (!idToEdit) {
       // create a new item
-      const id = crypto.randomUUID();
-      const todoObj = { id, title, description };
-      return setTodo([...todo, todoObj]);
+      let todoObj = { title, description };
+      const saveItem = await postNewTask(`${baseURL}/item`, todoObj);
+      if (saveItem.status === 200) {
+        console.log("hello");
+        todoObj["id"] = saveItem.data.id;
+        return setTodo([...todo, todoObj]);
+      }
+      return;
     }
-    // edit existing item
+    // edit existing itemTodo
+    let editedItem = {} as Todo;
     const newItems = todo.map((el) => {
       if (el.id === idToEdit) {
         el.title = title;
         el.description = description;
+        editedItem = el;
       }
       return el;
     });
-
-    setTodo(newItems);
+    const editItem = await postNewTask(`${baseURL}/item`, editedItem);
+    if (editItem.status === 200) {
+      return setTodo(newItems);
+    }
     return setIdToEdit("");
   }
 }
